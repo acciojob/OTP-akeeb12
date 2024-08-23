0fa7c5b55bedbd6b7d8aa755ac95343699e8980f
akeeb12 <112759560+akeeb12@users.noreply.github.com>
2024-08-23 17:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 //your JS code here. If required.
+document.addEventListener("DOMContenetLoaded",() =>{
 
-const codes = document.querySelectorAll('.code');
+const codes = document.querySelectorAll(".code");
 
 codes[0].focus();
 
 codes.forEach((code, idx) =>
 	{
-		code.addEventListener('keydown', (e) =>
+		code.addEventListener("keydown", (e) =>
 			{
-				if(e.key >= 0 && e.key <= 9)
+				if(target.value.length > 0 && idx < codes.length - 1)
 				{
-					codes[idx].value ='';
-					setTimeout(() => codes[idx + 1].focus(), 10);
-				} else if (e.key === 'Backspace') {
-					setTimeout(() => codes[idx - 1].focus(), 10);
+					codes[idx + 1].focus();
 				}
 			});
+		code.addEventListener("keydown", (e) =>
+			{
+				if(e.key == "Backspace" && idx > 0)
+				{
+					codes[idx - 1].focus;
+				}
+			});
+		});
+	
 	});
 
 
